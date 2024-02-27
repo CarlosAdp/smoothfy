3f0d6d062475691465c1026a01e4df2bc823f217
Carlos Pinto <carlos.adpinto@gmail.com>
2024-02-27 11:57:42 -0300

    Improve data presentation in DynamoDB

--- backend/assets/lambda/update_user_saved_tracks.py
+++ backend/assets/lambda/update_user_saved_tracks.py
@@ -44,14 +44,24 @@ def handler(event: dict, context: dict) -> dict:
             response = sp.current_user_saved_tracks(50, page_offset)
             total = response['total']
 
+            tracks = [
+                {
+                    'Id': track['track']['id'],
+                    'Name': track['track']['name'],
+                    'Artists': [
+                        {'Id': artist['id'], 'Name': artist['name']}
+                        for artist in track['track']['artists']
+                    ],
+                    'ReleaseYear': track['track']['album']['release_date'][:4],
+                }
+                for track in response['items']
+            ]
+
             item = {
                 'UserId': user_id,
                 'Offset': page_offset,
                 'Total': response['total'],
-                'Tracks': [[track['track']['id']] + [
-                    artist['id']
-                    for artist in track['track']['artists']
-                ] for track in response['items']],
+                'Tracks': tracks,
                 'TTL': int(time.time()) + 60 * 60 * 24,
             }
 
@@ -66,3 +76,20 @@ def handler(event: dict, context: dict) -> dict:
         "statusCode": 200,
         "body": "Tracks successfully saved"
     }
+
+
+if __name__ == '__main__':
+    from dotenv import load_dotenv
+
+    load_dotenv()
+
+    handler({
+        'Records': [
+            {
+                'messageAttributes': {
+                    'UserId': {'stringValue': os.getenv('USER_ID')},
+                    'AccessToken': {'stringValue': os.getenv('ACCESS_TOKEN')},
+                }
+            }
+        ]
+    }, None)
