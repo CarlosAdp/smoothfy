b2aea9390f500b6aaf06c6e30ce5095977c44161
Carlos Pinto <carlos.adpinto@gmail.com>
2024-03-06 17:45:06 -0300

    Add audio features to user saved tracks

--- backend/assets/lambda/update_user_saved_tracks.py
+++ backend/assets/lambda/update_user_saved_tracks.py
@@ -4,7 +4,11 @@ import os
 import time
 
 import awswrangler as wr
+import numpy as np
+import pandas as pd
 import spotipy
+
+from lib import audio_features
 
 
 logger = logging.getLogger()
@@ -44,17 +48,25 @@ def handler(event: dict, context: dict) -> dict:
             response = sp.current_user_saved_tracks(50, page_offset)
             total = response['total']
 
-            tracks = [
-                {
-                    'Id': track['track']['id'],
-                    'Name': track['track']['name'],
-                    'Artists': [
-                        {'Id': artist['id'], 'Name': artist['name']}
-                        for artist in track['track']['artists']
-                    ],
-                }
-                for track in response['items']
-            ]
+            track_ids = [track['track']['id'] for track in response['items']]
+            features = pd.DataFrame(filter(None, sp.audio_features(track_ids)))
+            features = audio_features.normalize_audio_features(features)
+
+            tracks = pd.json_normalize(response['items'])\
+                .loc[:, ['track.id', 'track.name', 'track.artists']]\
+                .rename(columns={
+                    'track.id': 'Id',
+                    'track.name': 'Name',
+                    'track.artists': 'Artists',
+                })\
+                .set_index('Id')\
+                .join(features, how='inner')\
+                .reset_index()
+
+            numeric_cols = tracks.select_dtypes(include=[np.number]).columns
+            tracks[numeric_cols] = (tracks[numeric_cols] * 1000).astype(int)
+
+            tracks = tracks.to_dict(orient='records')
 
             item = {
                 'UserId': user_id,
@@ -82,7 +94,7 @@ if __name__ == '__main__':
 
     load_dotenv()
 
-    handler({
+    print(handler({
         'Records': [
             {
                 'messageAttributes': {
@@ -91,4 +103,4 @@ if __name__ == '__main__':
                 }
             }
         ]
-    }, None)
+    }, None))
